5b64bb0f09abad27f508c06a7bb91c93bc4e7ed7
Samad Khan <samadk619@gmail.com>
2024-06-12 10:18:42 +0530

    test git commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <SignedOut>
-            {/* <SignInButton /> */}
+            <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
